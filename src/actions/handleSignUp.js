d1feca6d87666d4432b2e51dae13540445ed4d52
xibluespider <xibluespider@gmail.com>
2025-01-07 21:14:20 +0530

    fix: actions/handleSignUp: handle errors for username,email

--- src/actions/handleSignUp.js
+++ src/actions/handleSignUp.js
@@ -1,49 +1,40 @@
 "use server";
 
-import { addUser, getUserByEmail } from "@/lib/db";
+import { addUser } from "@/lib/db";
 import bcrypt from "bcryptjs";
 
 export default async function handleSignUp(credentials) {
-	const { name, email, password, confirmPassword } = credentials;
-
-	if (password !== confirmPassword)
-		return {
-			ok: false,
-			message: "Passwords do not match. Please sign up again",
-		};
-
-	let iuser = null;
-	try {
-		iuser = await getUserByEmail(email);
-	} catch (error) {
-		return {
-			ok: false,
-			message: "Internal server error. Please try again later",
-		};
-	}
-
-	if (iuser)
-		return {
-			ok: false,
-			message: "User already exists. Please sign in",
-		};
+	const { username, email, password, confirmPassword } = credentials;
 
 	const saltRounds = parseInt(process.env.SALT_ROUNDS);
 	const hashedPassword = bcrypt.hashSync(password, saltRounds);
 
 	const new_user_credentials = {
+		username,
 		email,
-		name,
 		password: hashedPassword,
 	};
 
 	try {
 		const response = await addUser(new_user_credentials);
 	} catch (error) {
-		return {
+		// if (error?.name !== "PostgresError") return response;
+		// if (error?.severity !== "ERROR") return response;
+
+		let message = "Internal server error. Please try again later";
+
+		if (error?.constraint_name === "users_email_unique")
+			message = "Email already exists. Please sign in";
+
+		if (error?.constraint_name === "users_username_unique")
+			message = "Username already exists.";
+
+		const response = {
 			ok: false,
-			message: "Internal server error. Please try again later",
+			message,
 		};
+
+		return response;
 	}
 
 	return {
