e562cda5ccefb3b7cbf6c0bc3a8ecb57db0a14f4
xibluespider <xibluespider@gmail.com>
2025-01-07 13:59:19 +0530

    fix: db-schema/users: rename name to username, with unique constraint

--- src/lib/db-schema.js
+++ src/lib/db-schema.js
@@ -2,7 +2,7 @@ import { pgTable, text, uuid } from "drizzle-orm/pg-core";
 
 export const users = pgTable("users", {
 	id: uuid("id").defaultRandom().primaryKey(),
-	name: text("name").notNull(),
+	username: text("username").notNull().unique(),
 	email: text("email").notNull().unique(),
 	password: text("password").notNull(),
 });
