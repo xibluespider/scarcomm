5eb7564318b784ac9d09263fe9555c331700f284
xibluespider <xibluespider@gmail.com>
2025-01-07 13:58:28 +0530

    chore: db-schema: prettier format

--- src/lib/db-schema.js
+++ src/lib/db-schema.js
@@ -1,8 +1,8 @@
 import { pgTable, text, uuid } from "drizzle-orm/pg-core";
 
 export const users = pgTable("users", {
-  id: uuid("id").defaultRandom().primaryKey(),
-  email: text("email").notNull().unique(),
-  name: text("name").notNull(),
-  password: text("password").notNull(),
+	id: uuid("id").defaultRandom().primaryKey(),
+	email: text("email").notNull().unique(),
+	name: text("name").notNull(),
+	password: text("password").notNull(),
 });
