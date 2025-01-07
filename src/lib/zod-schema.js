c39c232ff57fb97d7806aa94fe9937c5b4fd3b5a
xibluespider <xibluespider@gmail.com>
2025-01-07 14:19:39 +0530

    chore: zod-schema/signUpSchema: add min 3 chars condition to username

--- src/lib/zod-schema.js
+++ src/lib/zod-schema.js
@@ -2,7 +2,7 @@ import { z } from "zod";
 
 export const signUpSchema = z
   .object({
-    username: z.string().min(1, { message: "Username is required" }),
+    username: z.string().min(3, { message: "Username must be at least 3 characters" }),
     email: z.string().email({ message: "Invalid email address" }),
     password: z
       .string()
