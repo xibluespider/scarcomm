9c4614ab1a426487e29f31ca949d4561e35d3ce3
xibluespider <xibluespider@gmail.com>
2025-01-07 14:18:25 +0530

    fix: zod-schema/signUpSchema: replace name with username.

--- src/lib/zod-schema.js
+++ src/lib/zod-schema.js
@@ -2,7 +2,7 @@ import { z } from "zod";
 
 export const signUpSchema = z
   .object({
-    name: z.string().min(1, { message: "Name is required" }),
+    username: z.string().min(1, { message: "Username is required" }),
     email: z.string().email({ message: "Invalid email address" }),
     password: z
       .string()
