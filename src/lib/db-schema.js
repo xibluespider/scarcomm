9c71f9fb8e6affe3dc463cb02c4ac14d56897bf0
xibluespider <xibluespider@gmail.com>
2025-01-07 13:58:50 +0530

    chore: db-schema: reorder users schema

--- src/lib/db-schema.js
+++ src/lib/db-schema.js
@@ -2,7 +2,7 @@ import { pgTable, text, uuid } from "drizzle-orm/pg-core";
 
 export const users = pgTable("users", {
 	id: uuid("id").defaultRandom().primaryKey(),
-	email: text("email").notNull().unique(),
 	name: text("name").notNull(),
+	email: text("email").notNull().unique(),
 	password: text("password").notNull(),
 });
