930443655861eaffffc827f0a46ab46c6b7a544d
xibluespider <xibluespider@gmail.com>
2025-01-10 13:25:22 +0530

    feat: db-schema: add globalMsgIdSeq, globalMessages

--- src/lib/db-schema.js
+++ src/lib/db-schema.js
@@ -1,8 +1,32 @@
-import { pgTable, text, uuid } from "drizzle-orm/pg-core";
+import {
+	integer,
+	pgSequence,
+	pgTable,
+	text,
+	timestamp,
+	uuid,
+} from "drizzle-orm/pg-core";
+import { sql } from "drizzle-orm";
 
 export const users = pgTable("users", {
 	id: uuid("id").defaultRandom().primaryKey(),
 	username: text("username").notNull().unique(),
 	email: text("email").notNull().unique(),
 	password: text("password").notNull(),
+});
+
+export const globalMsgIdSeq = pgSequence("global_msg_id_seq", {
+	startWith: 1,
+	increment: 1,
+	maxValue: 5,
+	cycle: true,
+});
+
+export const globalMessages = pgTable("global_messages", {
+	id: integer("id")
+		.primaryKey()
+		.default(sql`nextval('global_msg_id_seq')`),
+	createdAt: timestamp("created_at").notNull().defaultNow(),
+	userId: uuid("user_id").references(() => users.id),
+	message: text("message").notNull(),
 });
