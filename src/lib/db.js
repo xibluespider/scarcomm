0c9eb75993ae687c4369fca6789970b8dbcf257d
xibluespider <xibluespider@gmail.com>
2025-01-10 13:46:31 +0530

    refactor: db: use namespace imports for db-schema and drizzle-orm

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,30 +1,38 @@
 import postgres from "postgres";
 import { drizzle } from "drizzle-orm/postgres-js";
 
-import { users } from "./db-schema";
-import { eq, or, like } from "drizzle-orm";
+import * as schema from "./db-schema";
+import * as orm from "drizzle-orm";
 
 const pool = postgres(process.env.DATABASE_URL, { max: 1 });
 export const db = drizzle(pool);
 
 export async function getUserByEmail(email) {
-	const iusers = await db.select().from(users).where(eq(users.email, email));
+	const iusers = await db
+		.select()
+		.from(schema.users)
+		.where(orm.eq(schema.users.email, email));
 	const [user] = iusers;
 	return user;
 }
 
 export async function addUser(credentials) {
-	await db.insert(users).values(credentials);
+	await db.insert(schema.users).values(credentials);
 }
 
 export async function searchUser(query) {
 	const iusers = await db
 		.select({
-			name: users.name,
-			email: users.email,
+			username: schema.users.username,
+			email: schema.users.email,
 		})
-		.from(users)
-		.where(or(like(users.name, `%${query}%`), like(users.email, `%${query}%`)));
+		.from(schema.users)
+		.where(
+			orm.or(
+				orm.like(schema.users.username, `%${query}%`),
+				orm.like(schema.users.email, `%${query}%`)
+			)
+		);
 
 	return iusers;
 }
