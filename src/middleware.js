08021bbfc604f2c28307361d4cc9918d4a03241d
xibluespider <xibluespider@gmail.com>
2025-01-11 08:57:15 +0530

    fix: middleware: protect pages

--- src/middleware.js
+++ src/middleware.js
@@ -2,24 +2,38 @@ import { NextResponse } from "next/server";
 import { auth } from "./auth";
 
 export const imiddleware = (req) => {
-  const isAuthenticated = !!req.auth;
-  const pathname = req.nextUrl.pathname;
+	const isAuthenticated = !!req.auth;
+	const pathname = req.nextUrl.pathname;
 
-  if (pathname === "/test") return NextResponse.next();
+	console.log(
+		`[MIDDLEWARE] Request to: ${pathname}, isAuthenticated: ${isAuthenticated}`
+	);
 
-  if (pathname === "/" && !isAuthenticated) {
-    return NextResponse.redirect(new URL("/auth", req.url));
-  }
+	if (pathname === "/about") {
+		console.log("[MIDDLEWARE] Path is /about. Allowing.");
+		return NextResponse.next();
+	}
 
-  if (pathname === "/auth" && isAuthenticated) {
-    return NextResponse.redirect(new URL("/", req.url));
-  }
+	if (pathname === "/auth" && isAuthenticated) {
+		console.log(
+			"[MIDDLEWARE] Path is /auth and user is authenticated. Redirecting to /."
+		);
+		return NextResponse.redirect(new URL("/", req.url));
+	}
 
-  return NextResponse.next();
+	if (pathname !== "/auth" && !isAuthenticated) {
+		console.log(
+			"[MIDDLEWARE] Path is not /auth and user is not authenticated. Redirecting to /auth."
+		);
+		return NextResponse.redirect(new URL("/auth", req.url));
+	}
+
+	console.log("[MIDDLEWARE] No conditions matched. Allowing.");
+	return NextResponse.next();
 };
 
 export default auth(imiddleware);
 
 export const config = {
-  matcher: ["/", "/auth", "/about", "/test"],
+	matcher: ["/", "/auth", "/about", "/global", "/channel/:path*"],
 };
