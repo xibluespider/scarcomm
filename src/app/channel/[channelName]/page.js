6a5fdf00ae55c81ad026416483805a8c00e9e6b8
xibluespider <xibluespider@gmail.com>
2025-01-11 11:44:37 +0530

    wip: channelName: progress

--- src/app/channel/[channelName]/page.js
+++ src/app/channel/[channelName]/page.js
@@ -1,10 +1,20 @@
 "use client";
 
+import LoadingPage from "@/app/loading";
+
+import useChannelEvents from "@/hooks/useChannelEvents";
+
 export default function Page({ params }) {
+	const { session, userCheckIsLoading } = useChannelEvents(params.channelName);
+
+	if (session.status == "loading") return <LoadingPage />;
+	if (userCheckIsLoading) return <LoadingPage />;
+
 	return (
 		<div className="grow border rounded-lg flex flex-col items-center justify-center">
 			<div>
 				<div className="text-xl">ChannelName: {params.channelName}</div>
+				<div>{JSON.stringify(userCheckIsLoading)}</div>
 			</div>
 		</div>
 	);
